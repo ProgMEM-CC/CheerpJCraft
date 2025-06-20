fe92c5509873d4086f75ffe929c7c4218a5c1b25
ProgMEMCC <progmem.cc@gmail.com>
2025-06-20 14:29:48 +0800

    stuff

---
+++ minecraft-web.js
@@ -0,0 +1,99 @@
+/**
+ * Downloads a file from a URL and writes it to the CheerpJ filesystem.
+ * @param {string} url
+ * @param {string} destPath
+ * @param {(downloadedBytes: number, totalBytes: number) => void} [progressCallback]
+ * @returns {Promise<void>}
+ */
+async function downloadFileToCheerpJ(url, destPath, progressCallback) {
+  const response = await fetch(url);
+  const reader = response.body.getReader();
+  const contentLength = +response.headers.get('Content-Length');
+
+  const bytes = new Uint8Array(contentLength);
+  progressCallback?.(0, contentLength);
+
+  let pos = 0;
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    bytes.set(value, pos);
+    pos += value.length;
+    progressCallback?.(pos, contentLength);
+  }
+
+  // Write to CheerpJ filesystem
+  return new Promise((resolve, reject) => {
+    var fds = [];
+    cheerpOSOpen(fds, destPath, "w", fd => {
+      cheerpOSWrite(fds, fd, bytes, 0, bytes.length, w => {
+        cheerpOSClose(fds, fd, resolve);
+      });
+    });
+  });
+}
+
+export default class MinecraftClient {
+  #progress;
+  #button;
+  #display;
+  #intro;
+  #isRunning;
+
+  constructor() {
+    this.#button = document.querySelector('button');
+    this.#button.addEventListener('click', () => this.run());
+
+    this.#progress = document.querySelector('progress');
+    this.#progress.style.display = 'none';
+
+    this.#intro = document.querySelector('.intro');
+
+    // CheerpJ needs an element to render to
+    this.#display = document.querySelector('.display');
+    cheerpjCreateDisplay(-1, -1, this.#display);
+
+    this.#isRunning = false;
+  }
+
+  /** @returns {Promise<number>} Exit code */
+  async run() {
+    if (this.#isRunning) {
+      throw new Error('Already running');
+    }
+
+    if (self.plausible)
+      self.plausible("Play");
+
+    this.#intro.style.display = 'none';
+    this.#progress.style.display = 'unset';
+
+    /*
+    const jarPath = "/files/client_1.2.5.jar";
+    await downloadFileToCheerpJ(
+      "https://archive.org/download/Minecraft-JE-Beta/Prereleases/b1.6-tb3/b1.6-tb3.jar",
+      jarPath,
+      (downloadedBytes, totalBytes) => {
+        this.#progress.value = downloadedBytes;
+        this.#progress.max = totalBytes;
+      }
+    );
+    */
+
+    this.#progress.style.display = 'none';
+    this.#display.style.display = 'unset';
+
+    const exitCode = await cheerpjRunMain(
+      "net.minecraft.client.Minecraft",
+      `/app/lwjgl-2.9.3.jar:/app/lwjgl_util-2.9.3.jar:/app/client.jar`
+    );
+
+    this.#isRunning = false;
+    return exitCode;
+  }
+
+  /** @returns {boolean} */
+  get isRunning() {
+    return this.#isRunning;
+  }
+}
